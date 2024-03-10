454a11f413a354f434e4e7792ba0670666245489
github xxbbxx825 <xxbbxx825@gmail.com>
2024-03-10 12:06:01 +0900

    Add some core rosources

---
+++ practice-gcp/terraform/modules/core/google_cloud_run.tf
@@ -0,0 +1,54 @@
+resource "google_cloud_run_v2_service" "web-server" {
+  name     = "next-web"
+  location = var.region
+  template {
+    containers {
+      image = "${google_artifact_registry_repository.docker.location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker.name}/next-web:latest"
+      resources {
+        limits = {
+          cpu    = "2"
+          memory = "1024Mi"
+        }
+      }
+
+      env {
+        name = "SECRET_ENV_VAR"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.secret.secret_id
+            version = "1"
+          }
+        }
+      }
+      volume_mounts {
+        name       = "cloudsql"
+        mount_path = "/cloudsql"
+      }
+    }
+
+
+
+  }
+  traffic {
+    percent = 100
+  }
+  depends_on = [google_secret_manager_secret_version.secret-version-data]
+}
+
+resource "google_secret_manager_secret" "secret" {
+  secret_id = "secret-1"
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  replication {
+    auto {
+
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret-version-data" {
+  secret      = google_secret_manager_secret.secret.name
+  secret_data = "secret-data"
+}
--- practice-gcp/terraform/modules/core/google_project_service.tf
+++ practice-gcp/terraform/modules/core/google_project_service.tf
@@ -13,3 +13,8 @@ resource "google_project_service" "artifactregistry" {
   service            = "artifactregistry.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "google_sql_database_instance" {
+  service            = "sql-component.googleapis.com"
+  disable_on_destroy = false
+}
